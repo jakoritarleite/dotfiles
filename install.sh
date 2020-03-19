5f0b00824430c83dbe0ba23c8388d7d0112f866f
João Koritar <jakoritarleite@gmail.com>
2020-03-19 11:45:21 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -26,7 +26,7 @@ echo "Configuring zsh preferences:"
 
 echo "Installing Spaceship:"
 sudo git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
-sudo ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+sudo ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$HOME/.oh-my-zsh/themes/spaceship.zsh-theme"
 
 #echo "Installing Zplugin's"
 #sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zplugin/master/doc/install.sh)"
