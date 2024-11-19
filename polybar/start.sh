e22b107388abd0c32defc1673bbb2ec78baa9718
João Koritar <jakoritarleite@gmail.com>
2024-11-19 09:26:57 -0300

    Update polybar

--- polybar/start.sh
+++ polybar/start.sh
@@ -6,5 +6,14 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+is_hdmi_connected=$(xrandr |grep ' connected' |grep 'HDMI' |awk '{print $1}')
+
+if [[ ! -z "$is_hdmi_connected" ]]; then
+    polybar bottom_hdmi -c $HOME/.config/polybar/config.ini
+else
+    polybar bottom_auto -c $HOME/.config/polybar/config.ini
+fi
+
 # Launch polybar
-polybar -c $HOME/.config/polybar/config.ini
+# polybar -c $HOME/.config/polybar/config.ini
+
